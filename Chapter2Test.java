5a7794d56e2b053361e4754d1ff1a5b90a85707f
ROCCO TAMMONE <rt0726@students.necsd.net>
2020-11-12 15:09:31 +0000

    lab done

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,22 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
-  
+  Scanner scan = new Scanner(System.in);
+
+  System.out.println("Enter a number: ");
+  int num1= scan.nextInt();
+
+  System.out.println("Enter a number: ");
+  int num2= scan.nextInt();
+
+  double num11 =  (double) num1;
+  double num22 =  (double) num2;
+
+
+  double x = (num11 + num22) / 2;
+
+  System.out.println("The average is: " + x);
+
   }
 }
